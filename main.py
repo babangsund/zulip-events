8e877524194b0971d6e57cae60c3d9c16e51aae5
babangsund <babangsund@gmail.com>
2019-12-06 23:03:49 +0100

    main method, react to all with monkey emoji :monkey:

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import sys
+
 import zulip
 
 # Pass the path to your zuliprc file here.
 client = zulip.Client(config_file="./.zuliprc")
 
 
+def react_monkey(msg):
+    """ React to all messages with the :monkey: emoji """
+    client.add_reaction({"message_id": msg["id"], "emoji_name": "monkey"})
+
+
 def react(msg):
     try:
-        print(msg)
+        sys.stdout.write(str(msg))
+        react_monkey(msg)
     except Exception:
         print("Caught exception")
 
 
 # Message subscription
-client.call_on_each_message(lambda msg: react(msg))
+def main():
+    client.call_on_each_message(lambda msg: react(msg))
